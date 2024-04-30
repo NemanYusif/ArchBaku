3bbd5c5cec8482a00d9977da4289a9283954e2ae
Your Name <nemanyusif05@gmail.com>
2024-04-30 19:36:34 +0400

    NewsDetailPage

--- src/Pages/News/index.jsx
+++ src/Pages/News/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import TopHeader from "../../Components/TopHeader";
+import Header from "../../Components/Header";
+import NewsBanner from "../../Components/NewsBanner";
+import NewsDetail from "../../Components/NewsDetail";
+import Footer from "../../Components/Footer";
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <>
+      <TopHeader />
+      <Header />
+      <NewsBanner />
+      <NewsDetail />
+      <Footer />
+    </>
+  );
 };
 
 export default News;
